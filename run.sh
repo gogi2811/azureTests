7e8297eba8243610d578cc7ef17b835a297e9d49
Gaurav Lotekar <gauravnlotekar@gmail.com>
2024-06-22 23:19:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 apt-get update &&
 apt-get install -y apt-utils &&
-apt-get install libzbar0
-apt-get install -y python-waitress
-echo "HEllO"
-waitress-serve --listen=*:8000 startup:app
+apt-get install libzbar0 &&
+apt-get install -y python-waitress &&
+echo "HEllO" &&
+# Install Python packages from requirements.txt
+pip3 install -r requirements.txt &&
+waitress-serve --listen=*:8000 app:app
