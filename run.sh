19148412d46ab8293ac51789644d7c35a350e95d
Gaurav Lotekar <gauravnlotekar@gmail.com>
2024-06-23 00:26:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update &&
 apt-get install -y apt-utils &&
-apt-get install libzbar0 &&
+apt-get install -y libzbar0 &&
 apt-get install -y python-waitress &&
 echo "HEllO" &&
 # Install Python packages from requirements.txt
